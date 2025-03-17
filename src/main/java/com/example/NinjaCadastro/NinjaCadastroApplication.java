c3d190436b91fe0811b39492d1fec56982c48e51
JReis <jedor.reis@gmail.com>
2025-03-17 11:29:33 -0300

    first commit

---
+++ src/main/java/com/example/NinjaCadastro/NinjaCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.NinjaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NinjaCadastro/NinjaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NinjaCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
